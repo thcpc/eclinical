c40d89af447ab8d61b1cbe68c5842b829e46ee0e
thcpc <redcpc@163.com>
2022-03-21 14:00:23 +0800

    WHY: add the global conftest HOW: WHAT:

--- scaffold/command/edk.py
+++ scaffold/command/edk.py
@@ -20,19 +20,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-init", action='store_true', help="初始化整个项目，包括生成environment.yaml，全局的conftest")
 parser.add_argument("-new", action='store_true', help="新增加 environment 到 environment.yaml中 ")
 parser.add_argument("-no_db", action='store_true', help="不需要配置数据库")
+parser.add_argument("-no_test", action='store_true', help="不成生全局的conftest")
 args = parser.parse_args()
 
 
 class Edk:
     def __init__(self):
         self.environment_path = os.path.join(os.getcwd(), "environment.yaml")
+        self.conftest_path = os.path.join(os.getcwd(), "conftest.py")
 
     def init(self):
-        shutil.copyfile(os.path.join(os.path.dirname(__file__), "resource", "init_environment.yaml"), self.environment_path)
-        self.generate_conftest()
+        shutil.copyfile(os.path.join(os.path.dirname(__file__), "resource", "init_environment.yaml"),
+                        self.environment_path)
 
-    # TODO 生成 conftest
-    def generate_conftest(self): pass
+    def generate_conftest(self):
+        shutil.copyfile(os.path.join(os.path.dirname(__file__), "resource", "global_conftest"), self.conftest_path)
 
     def add_environment(self, name: str, environment: dict):
         with open(self.environment_path, 'r', encoding="utf-8") as f:
@@ -56,38 +58,44 @@ class Edk:
                 "38": "http://200.200.101.38/api",
                 "us_dev": "https://ec.eclinical-dev.edetekapps.com/api",
                 "us_prod": "https://ec.eclinical.edetekapps.com/api",
-                "us_demo4": "",#TODO 添加地址
+                "us_demo4": "https://ec4.ec4demo.edetekapps.com/api",
                 "shenkang_prod": "https://crip-ec.shdc.org.cn/api"}
 
 
 def edk_command():
-    edk = Edk()
-    if args.init:
-        if edk.exist(): raise Exception("已初始化，请勿重复初始化")
-        edk.init()
-    if args.new:
-        if not edk.exist(): raise Exception("请先执行-init 初始化，再执行-new")
-        data = dict()
-        environment_name = EnvironmentName.input(edk)
-        data["uri"] = EclinicalUri.input(edk)
-        data["user"] = Account.input()
-        data["password"] = PassWord.input("请输入用户名密码", "Admin@123", "回车")
-        data["publick_key"] = input("加密公钥\n")
-        data["company"] = Company.input("请输入公司名", "非Cross账号", "回车")
+    try:
+        edk = Edk()
+        if args.init:
+            if edk.exist():
+                raise Exception("已初始化，请勿重复初始化")
+            edk.init()
+            if not args.no_test:
+                edk.generate_conftest()
+        if args.new:
+            if not edk.exist(): raise Exception("请先执行-init 初始化，再执行-new")
+            data = dict()
+            environment_name = EnvironmentName.input(edk)
+            data["uri"] = EclinicalUri.input(edk)
+            data["user"] = Account.input()
+            data["password"] = PassWord.input("请输入用户名密码", "Admin@123", "回车")
+            data["publick_key"] = input("加密公钥\n")
+            data["company"] = Company.input("请输入公司名", "非Cross账号", "回车")
 
-        data["sponsor"] = Sponsor.input()
-        data["study"] = Study.input("请输入实验名", "ctms, pv, etmf", "回车")
-        data["life_cycle"] = LifeCycle.input()
-        if not args.no_db:
-            data["db"] = dict()
+            data["sponsor"] = Sponsor.input()
+            data["study"] = Study.input("请输入实验名", "ctms, pv, etmf", "回车")
+            data["life_cycle"] = LifeCycle.input()
+            if not args.no_db:
+                data["db"] = dict()
 
-            data["db"]["host"] = DBHost.input()
-            data["db"]["user"] = Account.input()
-            data["db"]["pwd"] = PassWord.input("请输入数据库密码")
+                data["db"]["host"] = DBHost.input()
+                data["db"]["user"] = Account.input()
+                data["db"]["pwd"] = PassWord.input("请输入数据库密码")
 
-            data["db"]["database"] = Schema.input()
-            data["db"]["port"] = Port.input("请输入访问端口号", 3306, "回车")
+                data["db"]["database"] = Schema.input()
+                data["db"]["port"] = Port.input("请输入访问端口号", 3306, "回车")
             edk.add_environment(environment_name, data)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
