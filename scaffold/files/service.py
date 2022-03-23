cca0e97bc6b8536c2f12d4b154d18d746207a3ef
thcpc <redcpc@163.com>
2022-03-23 21:45:02 +0800

    WHY: 增加了不继承application的情况 HOW: WHAT:

--- scaffold/files/service.py
+++ scaffold/files/service.py
@@ -20,19 +20,22 @@ class Service:
         return clazz
 
     def output_conftest_py(self):
-        return f"from .service.{self.file_name} import {self.clazz_name}\n\n\n" +\
-               f"@pytest.fixture\n" + f"def {self.file_name}(ef, st):\n" +\
+        return "import pytest\n\n" + \
+               "from environment.environment import Environment\n\n" + \
+               f"from .service.{self.file_name} import {self.clazz_name}\n\n\n" + \
+               f"@pytest.fixture\n" + f"def {self.file_name}(ef, st):\n" + \
                f"\treturn {self.clazz_name}(Environment(envir=st, file_path=ef))\n\n\n"
 
     def output_init_py(self): return f"from .{self.file_name} import {self.clazz_name}\n"
 
     def output_service_py(self):
-        # TODO 修改添加的BigTangerine的导入
-        return "import cjen\n\n" + \
-               f"from eclinical import {self.clazz_name}, Environment\n\n"+\
-               f"class {self.clazz_name}({self.application_clazz}):\n"+\
-               f"\tdef __init__(self, environment: Environment):\n"+\
-               f"\tsuper().__init__(environment)\n" +\
-               f"\tself.context['cursor'] = " # TODO 添加数据库的代码
 
-
+        if self.application_clazz == "BigTangerine":
+            import_str = "import cjen\n\nfrom cjen import BigTangerine,DatabasePool\nfrom eclinical import Environment\n\n\n"
+        else:
+            import_str = f"import cjen\n\nfrom cjen import DatabasePool\nfrom eclinical import {self.application_clazz}, Environment\n\n\n"
+        return import_str + \
+               f"class {self.clazz_name}({self.application_clazz}):\n" + \
+               f"\tdef __init__(self, environment: Environment):\n" + \
+               f"\t\tsuper().__init__(environment)\n" + \
+               f"\t\tself.context['cursor'] = DatabasePool.cursor(host=environment.db.get(\"host\"), user=environment.db.get(\"user\"), pwd=environment.db.get(\"pwd\"), port=environment.db.get(\"port\"), database=environment.db.get(\"database\"))\n\n"
