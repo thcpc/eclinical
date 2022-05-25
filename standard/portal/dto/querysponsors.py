7f8c86791515ab52e3df02506bed3780d315cda1
thcpc <redcpc@163.com>
2022-05-25 18:53:16 +0800

    What: 造测试数据

--- standard/portal/dto/querysponsors.py
+++ standard/portal/dto/querysponsors.py
@@ -16,7 +16,7 @@ class Sponsor(MetaJson):
     def active(self): ...
 
 
-class Hierarchies(MetaJson):
+class QuerySponsors(MetaJson):
     @cjen.operate.asserts.equal(value=200)
     @cjen.operate.json.one(json_path="$.procCode")
     def procCode(self): ...
--- standard/steps/CreateStudy.py
+++
@@ -1,9 +0,0 @@
-from eclinical.standard.base.standard_step import StandardStep
-from eclinical.standard.portal.hierarchies import Hierarchies
-
-
-class CreateStudy(StandardStep):
-    def __int__(self, name, service: Hierarchies):
-        super(CreateStudy, self).__int__(name, service)
-
-    def run(self): ...
