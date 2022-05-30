3029b76cbedf6ef970c462d24478789a215fac98
thcpc <redcpc@163.com>
2022-05-30 20:34:25 +0800

    What: add the eDiary login service

---
+++ service/ediary_login_service.py
@@ -0,0 +1,25 @@
+import cjen
+from cjen import BigTangerine
+
+import eclinical
+
+
+class EDiaryLoginService(BigTangerine):
+    @cjen.headers.basicHeaders(headers={
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+    })
+    def __init__(self, environment: eclinical.Environment):
+        super().__init__()
+        self.environment = environment
+        self.base_url = self.__dict__.get("base_url", self.environment.uri)
+        self.context["userName"] = self.context.get("userName") or self.environment.user
+        self.context["password"] = self.context.get("password") or self.environment.password
+
+    @cjen.http.post_mapping(uri="admin/auth/subject/login")
+    @cjen.jwt(key="Authorization", json_path="$.payload.jwtAuthenticationResponse.token")
+    def auth(self, path_variable, data, resp=None, **kwargs): ...
+
+    @cjen.http.post_mapping(uri="edc/subject/auth/login")
+    @cjen.jwt(key="Authorization", json_path="$.payload.jwtAuthenticationResponse.token")
+    def edc_auth(self, resp=None, **kwargs): ...
